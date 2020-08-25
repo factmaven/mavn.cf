0909539fa26e3fa9ab51a4e8c5e1d9a20a0af563
Ethan Jinks O'Sullivan <ethanosullivan@users.noreply.github.com>
2020-08-25 10:01:19 -0400

    Fixed custom alias field

--- js/index.js
+++ js/index.js
@@ -3,35 +3,44 @@ let pushJSON = (address, longurl, shorturl) => {
     request.open('POST', address);
     request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     let data = {
-        "url": longurl,
-        "alias": shorturl
+        "l": longurl,
+        "s": shorturl
     };
     request.send(JSON.stringify(data));
 };
 
 let cinp = () => {
-    document.getElementById("url-error").innerHTML = "";
-    let cival = document.getElementById("custom-alias").value;
-
-    let res = JSON.parse(fetchJSON(endpoint + '/?q=alias:' + cival))[0]["url"];
+    document.getElementById("erbox").innerHTML = "";
+    let cival = document.getElementById("custominput").value;
+    
+    let res = JSON.parse(fetchJSON(endpoint + '/?q=s:' + cival))
+    if (res.length === 0){
+        return true;
+    }
+    res = res[0]["l"]
     let data = res;
+
 
     if (data != null) {
         return false;
+
     } else if (data == null) {
         return true;
+
     }
+
 
 };
 
 let geturl = () => {
-    let url = document.getElementById("long-url").value;
+    let url = document.getElementById("urlinput").value;
     return url;
+
 };
 
 let getrandom = () => {
     let text = "";
-    let possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     for (let i = 0; i < 5; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -39,22 +48,25 @@ let getrandom = () => {
 };
 
 let genhash = () => {
-    if (document.getElementById("custom-alias").value == "") {
+    if (document.getElementById("custominput").value == "") {
         window.location.hash = getrandom();
         check_is_unique();
     } else {
-        window.location.hash = document.getElementById("custom-alias").value;
+        window.location.hash = document.getElementById("custominput").value;
+
     }
 };
 
 let check_is_unique = () => {
     let url = window.location.hash.substr(1);
-    let res = JSON.parse(fetchJSON(endpoint + '/?q=alias:' + url))[0];
+    let res = JSON.parse(fetchJSON(endpoint + '/?q=s:' + url))[0];
     let data = res;
 
     if (data != null) {
         genhash();
     }
+
+
 };
 
 let copyer = (containerid) => {
@@ -69,6 +81,7 @@ let copyer = (containerid) => {
             range.select();
             document.execCommand("copy");
         }
+
     } else if (window.getSelection) {
         if (elt.nodeName.toLowerCase() === "input") {
             document.getElementById(containerid).select();
@@ -89,44 +102,60 @@ let send_request = (url) => {
     let address = endpoint + "/";
     // console.log(address)
     pushJSON(address, longurl, shorturl);
-    document.getElementById('short-url').value = window.location.href;
+
+    document.getElementById('shortenedURL').value = window.location.href;
     document.getElementById('sucess').innerHTML = "Short URL Copied to Clipboard!";
-    copyer("short-url");
+    copyer("shortenedURL");
 };
 
 let shorturl = () => {
     let longurl = geturl();
-    let regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+    let re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
     let cre = /^([a-zA-Z0-9 _-]+)$/;
-    let protocol_ok = regex.test(longurl);
+    let protocol_ok = re.test(longurl);
     if (!protocol_ok) {
-        document.getElementById("url-error").style.color = "red";
-        document.getElementById("url-error").innerHTML = "❌ Invalid URL";
+        document.getElementById("erbox").style.color = "red";
+        document.getElementById("erbox").innerHTML = "❌ Invalid URL";
     } else {
-        document.getElementById("url-error").innerHTML = "";
-        if (document.getElementById("custom-alias").value == "") {
+        document.getElementById("erbox").innerHTML = "";
+        if (document.getElementById("custominput").value == "") {
             genhash();
             send_request(longurl);
+
         } else {
-            if (cre.test(document.getElementById("custom-alias").value)) {
+            if (cre.test(document.getElementById("custominput").value)) {
                 if (cinp()) {
-                    document.getElementById("url-error").style.color = "cyan";
-                    document.getElementById("url-error").innerHTML = " Custom Address Available ✔️";
+                    document.getElementById("erbox").style.color = "cyan";
+                    document.getElementById("erbox").innerHTML = " Custom Address Available ✔️";
                     genhash();
                     send_request(longurl);
                 } else {
-                    document.getElementById("url-error").style.color = "red";
-                    document.getElementById("url-error").innerHTML = "❌ Custom Address Already Used, Choose Another";
-                    document.getElementById("custom-alias").placeholder = document.getElementById("custom-alias").value;
-                    document.getElementById("custom-alias").value = "";
+                    document.getElementById("erbox").style.color = "red";
+                    document.getElementById("erbox").innerHTML = "❌ Custom Address Already Used, Choose Another";
+                    document.getElementById("custominput").placeholder = document.getElementById("custominput").value;
+                    document.getElementById("custominput").value = "";
                 }
             } else {
-                document.getElementById("url-error").style.color = "red";
-                document.getElementById("url-error").innerHTML = "Invalid Custom URL! Use only Alphanumerics and underscore!";
-                document.getElementById("custom-alias").placeholder = document.getElementById("custom-alias").value;
-                document.getElementById("custom-alias").value = "";
+                document.getElementById("erbox").style.color = "red";
+                document.getElementById("erbox").innerHTML = "Invalid Custom URL! Use only Alphanumerics and underscore!";
+                document.getElementById("custominput").placeholder = document.getElementById("custominput").value;
+                document.getElementById("custominput").value = "";
+
             }
         }
+
+
     }
 };
-document.getElementById("shorten-url").addEventListener("click", shorturl);
+document.getElementById("sbtn").addEventListener("click", shorturl);
+
+console.log(`
+███████╗ ██████╗ ███████╗███████╗██╗   ██╗██████╗ ██╗     
+██╔════╝██╔═══██╗██╔════╝██╔════╝██║   ██║██╔══██╗██║     
+█████╗  ██║   ██║███████╗███████╗██║   ██║██████╔╝██║     
+██╔══╝  ██║   ██║╚════██║╚════██║██║   ██║██╔══██╗██║     
+██║     ╚██████╔╝███████║███████║╚██████╔╝██║  ██║███████╗
+╚═╝      ╚═════╝ ╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝
+                                                                                                                               
+PLEASE DON'T TYPE ANYTHING BELLOW UNLESS YOU ARE A DEVELOPER!
+`)
