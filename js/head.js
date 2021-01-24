673c778557dcca1de631aed29e7d90bda6507851
Ethan Jinks O'Sullivan <ethanosullivan@users.noreply.github.com>
2021-01-24 18:17:41 -0500

    Replace double with single quotes

--- js/head.js
+++ js/head.js
@@ -1,24 +1,24 @@
-var endpoint = "https://jsonbox.io/box_0394827869549f2a7393";
+var endpoint = 'https://jsonbox.io/box_0394827869549f2a7393';
 
 function getJsonbox(urlRequest) {
     var request = new XMLHttpRequest;
-    request.open("GET", urlRequest, false);
+    request.open('GET', urlRequest, false);
     request.send(null);
     return request.responseText;
 }
 
 function isURL(urlRequest) {
     let url = urlRequest
-    if (!urlRequest.startsWith("javascript:")) {
+    if (!urlRequest.startsWith('javascript:')) {
         return true;
     } else {
         return false;
     }
 }
 var hashtag = window.location.hash.substr(1);
-if (window.location.hash != "") {
-    var jsonReponse = JSON.parse(getJsonbox(endpoint + "/?q=alias:" + hashtag))[0];
-    var data = jsonReponse["url"];
+if (window.location.hash != '') {
+    var jsonReponse = JSON.parse(getJsonbox(endpoint + '/?q=alias:' + hashtag))[0];
+    var data = jsonReponse['url'];
     console.log(data);
     if (data != null) {
         if (isURL(data)) {
--- js/url-shortener.js
+++ js/url-shortener.js
@@ -1,23 +1,23 @@
 let postJsonbox = (address, longUrl, shortUrl) => {
     let request = new XMLHttpRequest();
-    request.open("POST", address);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.open('POST', address);
+    request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     let data = {
-        "url": longUrl,
-        "alias": shortUrl,
-        "domain": document.location.origin
+        'url': longUrl,
+        'alias': shortUrl,
+        'domain': document.location.origin
     };
     request.send(JSON.stringify(data));
 };
 
 let checkCustomUrlAlias = () => {
-    document.getElementById("error-message").innerHTML = "";
-    let customAlias = document.getElementById("url-alias").value;
-    let jsonResponse = JSON.parse(getJsonbox(endpoint + "/?q=alias:" + customAlias));
+    document.getElementById('error-message').innerHTML = '';
+    let customAlias = document.getElementById('url-alias').value;
+    let jsonResponse = JSON.parse(getJsonbox(endpoint + '/?q=alias:' + customAlias));
     if (jsonResponse.length === 0){
         return true;
     }
-    jsonResponse = jsonResponse[0]["url"]
+    jsonResponse = jsonResponse[0]['url']
     let data = jsonResponse;
     if (data != null) {
         return false;
@@ -27,13 +27,13 @@ let checkCustomUrlAlias = () => {
 };
 
 let getUrl = () => {
-    let url = document.getElementById("long-url").value;
+    let url = document.getElementById('long-url').value;
     return url;
 };
 
 let generateShortcode = () => {
-    let shortcode = "";
-    let possibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let shortcode = '';
+    let possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (let i = 0; i < 5; i++) {
         shortcode += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
     }
@@ -41,17 +41,17 @@ let generateShortcode = () => {
 };
 
 let generateHash = () => {
-    if (document.getElementById("url-alias").value == "") {
+    if (document.getElementById('url-alias').value == '') {
         window.location.hash = generateShortcode();
         checkIsUnique();
     } else {
-        window.location.hash = document.getElementById("url-alias").value;
+        window.location.hash = document.getElementById('url-alias').value;
     }
 };
 
 let checkIsUnique = () => {
     let url = window.location.hash.substr(1);
-    let jsonResponse = JSON.parse(getJsonbox(endpoint + "/?q=alias:" + url))[0];
+    let jsonResponse = JSON.parse(getJsonbox(endpoint + '/?q=alias:' + url))[0];
     let data = jsonResponse;
     if (data != null) {
         generateHash();
@@ -61,25 +61,25 @@ let checkIsUnique = () => {
 let copyToClipboard = (containerid) => {
     let elementType = document.getElementById(containerid);
     if (document.selection) { // IE
-        if (elementType.nodeName.toLowerCase() === "input") {
+        if (elementType.nodeName.toLowerCase() === 'input') {
             document.getElementById(containerid).select();
-            document.execCommand("copy");
+            document.execCommand('copy');
         } else {
             let range = document.body.createTextRange();
             range.moveToElementText(document.getElementById(containerid));
             range.select();
-            document.execCommand("copy");
+            document.execCommand('copy');
         }
     } else if (window.getSelection) {
-        if (elementType.nodeName.toLowerCase() === "input") {
+        if (elementType.nodeName.toLowerCase() === 'input') {
             document.getElementById(containerid).select();
-            document.execCommand("copy");
+            document.execCommand('copy');
         } else {
             let range_ = document.createRange();
             range_.selectNode(document.getElementById(containerid));
             window.getSelection().removeAllRanges();
             window.getSelection().addRange(range_);
-            document.execCommand("copy");
+            document.execCommand('copy');
         }
     }
 };
@@ -87,11 +87,11 @@ let copyToClipboard = (containerid) => {
 let sendRequest = (url) => {
     let longUrl = url;
     let shortUrl = window.location.hash.substr(1)
-    let address = endpoint + "/";
+    let address = endpoint + '/';
     postJsonbox(address, longUrl, shortUrl);
-    document.getElementById("short-url").value = window.location.href;
-    document.getElementById("sucess-message").innerHTML = "Short URL created and copied to clipboard";
-    copyToClipboard("short-url");
+    document.getElementById('short-url').value = window.location.href;
+    document.getElementById('sucess-message').innerHTML = 'Short URL created and copied to clipboard';
+    copyToClipboard('short-url');
 };
 
 let shortUrl = () => {
@@ -100,23 +100,23 @@ let shortUrl = () => {
     let customAliasRegex = /^([a-zA-Z0-9 _-]+)$/;
     let prococolValid = urlRegex.test(longUrl);
     if (!prococolValid) {
-        document.getElementById("error-message").innerHTML = "Invalid input. Make sure it starts with <code>http://</code> or <code>https://</code>";
+        document.getElementById('error-message').innerHTML = 'Invalid input. Make sure it starts with <code>http://</code> or <code>https://</code>';
     } else {
-        document.getElementById("error-message").innerHTML = "";
-        if (document.getElementById("url-alias").value == "") {
+        document.getElementById('error-message').innerHTML = '';
+        if (document.getElementById('url-alias').value == '') {
             generateHash();
             sendRequest(longUrl);
         } else {
-            if (customAliasRegex.test(document.getElementById("url-alias").value)) {
+            if (customAliasRegex.test(document.getElementById('url-alias').value)) {
                 if (checkCustomUrlAlias()) {
                     generateHash();
                     sendRequest(longUrl);
                 } else {
-                    document.getElementById("error-message").innerHTML = "Custom alias in use, choose another alias";                }
+                    document.getElementById('error-message').innerHTML = 'Custom alias in use, choose another alias';                }
             } else {
-                document.getElementById("error-message").innerHTML = "Use only alphanumerics and underscore";
+                document.getElementById('error-message').innerHTML = 'Use only alphanumerics and underscore';
             }
         }
     }
 };
-document.getElementById("submit-button").addEventListener("click", shortUrl);
+document.getElementById('submit-button').addEventListener('click', shortUrl);
